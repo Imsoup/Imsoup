caf844afca17de111cc6f293baca0a2a5c2935de
Imsoup <imre.detar@gmail.com>
2020-05-04 14:47:38 +0200

    toDoList

---
+++ scripts.js
@@ -0,0 +1,71 @@
+//Third part:
+console.log('Script file is loaded.');
+
+$('h1').html('This is done by <em>JavaScript!</em>');
+
+$('p').css('border', '1px solid red');
+$('p').css('padding', '10px');
+$('h2').toggleClass('blue');
+$('h2').toggleClass('blue');
+
+
+
+function myLittleFunction() {
+    //condition: number of ps is smaller than 5
+    if($('p').length < 5){
+        $('main').append('<p>A p is appended in the main </p>')
+    }else {
+        console.log('We reached the maximum paragraph number')
+    }
+    
+    $('p').css('border', '1px solid red');
+    $('p').css('padding', '10px');  
+    
+}
+myLittleFunction();
+
+
+$('#add').click(myLittleFunction);
+$('#remove').click(function(){
+    $('p:last-child').remove();
+});
+
+//Fourth part
+
+/*if (condition that true or false){
+    condition is true
+}else{
+    //condition is false
+}*/
+let colors=['orange', 'yellow', 'blue', 'lime', 'magenta'];
+colors.forEach(function (color){
+    $('#box-container').append('<div class="box"></div>');
+    $('.box:last-child').css('background', color);
+    
+});
+
+for (let i = 0; i<10; i++){
+    console.log("I'll not cheat on the exam.")
+    
+}
+
+
+/*l$('#box-container').append('<div id="box></div>');
+$('.box:last-child').css('background'), colors[0]);
+$('#box-container').append('<div id="box></div>');
+$('.box:last-child').css('background'), colors[1]);
+$('#box-container').append('<div id="box></div>');
+$('.box:last-child').css('background'), colors[2]);
+$('#box-container').append('<div id="box></div>');
+$('.box:last-child').css('background'), colors[3]);
+$('#box-container').append('<div id="box></div>');
+$('.box:last-child').css('background'), colors[4]);
+*/
+//Single line comment
+/*
+Multiple
+lines
+of comments
+*/
+
+//First part
